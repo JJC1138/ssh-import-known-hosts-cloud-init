74ffb808af3cb377b288ed8b645560841434e148
Jon Colverson <github@vcxz.co.uk>
2019-01-04 02:11:41 +0000

    Added finding of the public keys in the config file, and writing them out to the known_hosts file

--- sshimportknownhostscloudinit/__main__.py
+++ sshimportknownhostscloudinit/__main__.py
@@ -1,7 +1,12 @@
 import argparse
+import os.path
+import re
 import sys
 
 import ruamel.yaml
+
+def log(message):
+    print(message, file=sys.stderr)
 
 def main():
     arg_parser = argparse.ArgumentParser()
@@ -17,4 +22,27 @@ def main():
     if not args.hostname:
         args.hostname = config.get('fqdn') or config.get('hostname')
         if not args.hostname:
-            raise Exception("No hostname was found in the config file and none was given explicitly using the --hostname option")
+            raise Exception("No hostname was found in the config file and none was given explicitly using the --hostname option.")
+    
+    keys = config.get('ssh_keys')
+    
+    if not keys:
+        raise Exception("No 'ssh_keys' section was found in the config file.")
+    
+    public_key_type_pattern = re.compile('^(?P<key_type>.*?)_public$')
+    
+    with open(os.path.expanduser('~/.ssh/known_hosts'), 'at') as known_hosts_file:
+        for full_key_type in keys.keys():
+            match = public_key_type_pattern.match(full_key_type)
+            if not match:
+                continue
+            
+            key_type = match.group('key_type')
+            
+            key = keys[full_key_type]
+            
+            known_hosts_key_line = '%s %s' % (args.hostname, key)
+            
+            known_hosts_file.write(known_hosts_key_line)
+            
+            log("Imported %s key" % key_type)
