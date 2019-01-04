e6e98f63a483a14fd410d35e84cf1c8685665eeb
Jon Colverson <github@vcxz.co.uk>
2019-01-04 01:39:43 +0000

    Added finding of the hostname in the config file and erroring out if none is found and it isn't given as a program argument

--- sshimportknownhostscloudinit/__main__.py
+++ sshimportknownhostscloudinit/__main__.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+import ruamel.yaml
+
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--hostname', default=None, help="The hostname to use for the new key entries in the known_hosts file. If not given explictly then the hostname given in the config file's 'fqdn' or 'hostname' properties is used.")
@@ -8,3 +10,11 @@ def main():
     if len(sys.argv) == 1:
         sys.argv.append('-h')
     args = arg_parser.parse_args()
+    
+    with open(args.config_file_name, 'rb') as config_file:
+        config = ruamel.yaml.YAML(typ='safe').load(config_file)
+    
+    if not args.hostname:
+        args.hostname = config.get('fqdn') or config.get('hostname')
+        if not args.hostname:
+            raise Exception("No hostname was found in the config file and none was given explicitly using the --hostname option")
